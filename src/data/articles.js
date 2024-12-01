44f53d54e3fd2c802bb251f766cbb1116986f0e5
abdessadekbirama <abdessadekbirama1@gmail.com>
2024-12-01 21:45:16 +0100

    Add files via upload

---
+++ src/data/articles.js
@@ -0,0 +1,59 @@
+export default [
+    {
+        title:"Article1",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates quidem hic similique, ullam aliquid a eveniet ut facere, molestias quo minima repellendus voluptate, quibusdam expedita maiores perspiciatis debitis?",
+        likes:34,
+        commentsCount:10,
+        shares:3,
+        comments:[
+            "great article?",
+            "i agree with you bro",
+            "good job i like your idea.",
+            "about the title you can do it very easy to learn bro for one hour at least."
+        ]
+    },
+    {
+        title:"Article2",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates quidem hic similique, ullam aliquid a eveniet ut facere, molestias quo minima repellendus voluptate, quibusdam expedita maiores perspiciatis debitis?",
+        likes:12,
+        commentsCount:120,
+        shares:30,
+        comments:[
+            "great article?",
+            "i agree with you bro",
+        ]
+    },
+    {
+        title:"Article3",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates quidem hic similique, ullam aliquid a eveniet ut facere, molestias quo minima repellendus voluptate, quibusdam expedita maiores perspiciatis debitis?",
+        likes:4,
+        commentsCount:1,
+        shares:1,
+        comments:[
+            "good job i like your idea.",
+            "about the title you can do it very easy to learn bro for one hour at least."
+        ]
+    },
+    {
+        title:"Article4",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates quidem hic similique, ullam aliquid a eveniet ut facere, molestias quo minima repellendus voluptate, quibusdam expedita maiores perspiciatis debitis?",
+        likes:340,
+        commentsCount:100,
+        shares:50,
+        comments:[
+            "great article?",
+            "i agree with you bro",
+        ]
+    },
+    {
+        title:"Article5",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates quidem hic similique, ullam aliquid a eveniet ut facere, molestias quo minima repellendus voluptate, quibusdam expedita maiores perspiciatis debitis?",
+        likes:10,
+        commentsCount:10,
+        shares:19,
+        comments:[
+            "great article?",
+        ]
+    },
+    
+]
