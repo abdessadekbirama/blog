fea93d53a3e58e29f6075334bf6508182c2b6db6
abdessadek birama <abdessadekbirama1@gmail.com>
2024-12-14 19:34:04 +0100

    Update ArticleCard.jsx

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -12,7 +12,7 @@ function ArticleCard({img,title,content,publishDate,src,url}){
     }
     return (
         <div className="border-2 rounded border-[#d3e08b] flex flex-col gap-2 my-2 p-2 sm:w-2/3 m-auto">
-            <img className="block rounded bg-[url(/bg-img.png)] bg-size-[100px] min-w-20 min-h-20" src={img} alt=""/>
+            <img className="block rounded bg-[url(/bg-img.png)] min-w-full min-h-full" src={img} alt=""/>
             <h1 className="text-2xl mx-5">{title}</h1>
             <div>
                 <p className={`mx-5 text-ellipsis ${more?"":"line-clamp-2"}`}>{content}</p>
